12e7fc241084d2596f01f22ee9ac1d9a3a8f70a5
harun <harunnec@gmail.com>
2026-01-28 02:45:34 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from GCP DevOps Lab 🚀');
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
